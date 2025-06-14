fd538bb3c94a5ac6641fbc336fc363e262bfb571
Nurdaulet <nurdaulet.beketov.2005@gmail.com>
2025-06-14 22:29:41 +0500

    First commit

---
+++ src/main/java/com/grayball/GrayBall/GrayBallApplication.java
@@ -0,0 +1,13 @@
+package com.grayball.GrayBall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrayBallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrayBallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grayball/GrayBall/GrayBallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grayball.GrayBall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrayBallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
